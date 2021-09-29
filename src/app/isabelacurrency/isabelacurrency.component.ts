5bc2c04196b751b558c190c86a0f3e9216107cd5
isabeela <isabeelabg@gmail.com>
2021-09-29 19:42:54 -0300

    2 files modified

--- src/app/isabelacurrency/isabelacurrency.component.ts
+++ src/app/isabelacurrency/isabelacurrency.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IsabelawalletService } from '../isabelawallet.service';
 
 @Component({
   selector: 'app-isabelacurrency',
   templateUrl: './isabelacurrency.component.html',
-  styleUrls: ['./isabelacurrency.component.css']
+  styleUrls: ['./isabelacurrency.component.css'],
 })
-export class IsabelacurrencyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class IsabelacurrencyComponent {
+  ngOnInit() {}
 }
